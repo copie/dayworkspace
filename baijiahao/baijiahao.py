4b1789a02bd40917577c403b31fb4c053e0b5717
copie <691669873@qq.com>
2017-02-23 00:36:55 +0800

    完成部分抓取

--- baijiahao/baijiahao.py
+++ baijiahao/baijiahao.py
@@ -11,12 +11,14 @@ class sobaidu():
     '''sobaidu类实现通过百度搜索获取真实的url并且把url写入数据库'''
 
     def __init__(self):
-        self.FILENAME = "keylist.txt"
+        self.KEYFILENAME = "keylist.txt"
+        self.URLFILENAME = "urllist.txt"
         self.KEYLIST = set()
         self.URLLIST = set()
+        self.URLFILE = open(self.URLFILENAME, 'w')
     def _readkey(self):
         '''读取百度搜索所需要的所有关键词'''
-        with open(self.FILENAME) as keyklistfile:
+        with open(self.KEYFILENAME) as keyklistfile:
             for i in keyklistfile.readlines():
                 self.KEYLIST.add(i)
     def _changeurl(self,url):
@@ -34,6 +36,9 @@ class sobaidu():
     def _writetomysql(self):
         '''将真实url写入数据库'''
         pass
+    def _writetofile(self,url):
+        self.URLFILE.write(url)
+        self.URLFILE.write('\n')
 
     def sobaidu(self):
         '''调用以上函数解决我们的问题'''
@@ -45,16 +50,16 @@ class sobaidu():
             num += 1
             now_num = 0
             browser.implicitly_wait(30)
-            browser.get('https://www.baidu.com/s?wd=site:(baijiahao.baidu.com) '+ key )#+'inurl:( "http://baijiahao.baidu.com/u?app_id=" )')
-            if now_num == 1:
-                try:
-                    browser.find_element_by_xpath('//*[@id="page"]/a[10]').click()
-                    time.sleep(2)
-                except Exception as e:
-                    print(e)
-                    continue
-
+            browser.get('https://www.baidu.com/s?wd=site:(baijiahao.baidu.com) '+ key )
             while True:
+                if now_num == 1:
+                    try:
+                        browser.find_element_by_xpath('//*[@id="page"]/a[10]').click()
+                        time.sleep(2)
+                    except Exception as e:
+                        print(e)
+                        print("有问题")
+                        break
                 now_num += 1
                 print(now_num)
                 source = browser.page_source
@@ -63,7 +68,8 @@ class sobaidu():
                 for i in soup.findAll(class_='result c-container '):
                     url=i.find(class_='t').find('a').get('href')
                     # print(url)
-                    self.URLLIST.add(self._changeurl(url))
+                    # self.URLLIST.add(self._changeurl(url))
+                    self._writetofile(self._changeurl(url))
                 time.sleep(1)
                 if now_num > 1:
                     try:
@@ -73,7 +79,7 @@ class sobaidu():
                         print('not find next_button may be for the page end!!!')
                         break
 def main():
-    dsfsd = sobaidu()
+    dsfsd = sobaidu()    
     # strings = dsfsd._changeurl('https://www.baidu.com/link?url=w8wWEQMyVf0cD3TsKcn_pTQZ92cIqLqxVZKWFtT4rYJcESE_qfhKlPJg5B7OM2mXhZoSM1H0ogmCIgi4G2EkP_&wd=&eqid=aa2c3db90000bf4c0000000458831761') 
     # print(strings)
     # # ###################### 奇怪的分割线 ###############
@@ -81,8 +87,9 @@ def main():
     print(len(dsfsd.KEYLIST))
     #########################  有点傻的分割线 #############
     dsfsd.sobaidu()
-    print(len(dsfsd.URLLIST))
-    for i in dsfsd.URLLIST:
-        print(i)
+    # print(len(dsfsd.URLLIST))
+    # for i in dsfsd.URLLIST:
+    #     print(i)
+    dsfsd.URLFILE.close()
 if __name__ == '__main__':
     main()
