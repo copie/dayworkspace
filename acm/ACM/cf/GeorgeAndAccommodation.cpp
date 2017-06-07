a0a29a8bdbbccbf25b954a12f1df0b92a01689d8
copie <691669873@qq.com>
2017-06-07 12:53:13 +0800

    水题

---
+++ acm/ACM/cf/GeorgeAndAccommodation.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int sum=0;
+    while(n--)
+    {
+        int a,b;
+        cin>>a>>b;
+        if(b-a>=2)
+        sum++;
+    }
+    cout<<sum<<endl;
+}
---
+++ acm/ACM/cf/NearlyLuckNumber.py
@@ -0,0 +1,6 @@
+l = list(input())
+
+if l.count('4')+l.count('7') == 4 or l.count('4')+l.count('7')==7:
+    print('YES')
+else:
+    print("NO")
