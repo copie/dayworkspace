bc97c0c651c5237d36a04366ff42770ebb7c78a6
copie <copie@Lenovo.DHCP>
2017-06-12 21:05:34 +0800

    271A.Beautiful Year

---
+++ acm/ACM/cf/BeautifulYear.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    int a;
+    cin >> a;
+    a++;
+    while (1)
+    {
+        set<char> se;
+        stringstream ss;
+        string str;
+        ss << a;
+        ss >> str;
+        for (auto c : str)
+        {
+            se.insert(c);
+        }
+        if(se.size()==str.length())
+        {
+            cout<<str<<endl;
+            return 0;
+        }
+        else
+        {
+            a++;
+        }
+    }
+    return 0;
+}
