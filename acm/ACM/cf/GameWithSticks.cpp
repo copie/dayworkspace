1d713d6bfc61ccbaffdc600bf6aa8b6fca7aabf7
copie <copie@Lenovo.DHCP>
2017-06-13 10:53:11 +0800

    451AGame With Sticks

---
+++ acm/ACM/cf/GameWithSticks.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    int a,b;
+    cin>>a>>b;
+    a=min(a,b);
+    if(a%2==0)
+    cout<<"Malvika"<<endl;
+    else
+    cout<<"Akshat"<<endl;
+    return 0;
+}
