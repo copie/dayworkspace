2507d25a34bb778d9fafd86bee206d96de0b1800
copie <copie@Lenovo.DHCP>
2017-06-14 21:01:59 +0800

    牛客网pat第一题

---
+++ acm/ACM/pat/A+B和C.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        double a,b,c;
+        cin>>a>>b>>c;
+        cout<<"Case #"<<i+1<<": ";
+        if(a+b<=c)
+        cout<<"false"<<endl;
+        else
+        cout<<"true"<<endl;
+    }
+}
