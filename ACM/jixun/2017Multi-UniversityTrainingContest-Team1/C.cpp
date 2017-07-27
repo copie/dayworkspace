a8e20a6ca96ba2c4729a27c059424bb9cc789a0d
copie <admin@copie.cn>
2017-07-27 12:01:44 +0800

    整理目录

---
+++ ACM/jixun/2017Multi-UniversityTrainingContest-Team1/C.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int n;
+    while(cin>>n)
+    {
+        int color
+    }
+    return 0;
+}
--- test.cpp
+++
@@ -1,2 +0,0 @@
-sdfgjsdhfgjskd
-:w
