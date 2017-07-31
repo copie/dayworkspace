2a59af881a659903a9ac484236317a46863bd153
copie <admin@copie.cn>
2017-07-31 15:20:25 +0800

    给我的粘贴板添加上传文件的功能

--- python/codegui/service.py
+++ python/codegui/service.py
@@ -1,22 +1,36 @@
 from flask import Flask
 from flask import request
+import os
 app = Flask(__name__)
 code=''
+
+UPLOAD_FOLDER = '/home/copie/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 @app.route('/')
 def hello_world():
     return code
-@app.route("/hello")
-def haha():
-    return "这是一个测s "
 @app.route('/postcode', methods=['POST', 'GET'])
 def postcode():
     global code
     if request.method == "GET":
         code = request.args.get('code', '')
         return '200'
-    elif request.method == 'POST':
-        code = request.data
-
+@app.route('/upfile',methods=['POST'])
+def upfile():
+    if request.method == 'POST':
+            file = request.files['file']
+            if file and allowed_file(file.filename):
+                # filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], "ehwjfhsgd.txt"))
+                return "yes"
+            
+            return "200"
 if __name__ == '__main__':
     app.debug = True 
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     app.run(host='0.0.0.0')
--- test.cpp
+++ test.cpp
@@ -1,74 +1,63 @@
 #include <bits/stdc++.h>
+#define mod 1000000009
+typedef long long LL;
 
 using namespace std;
 
-typedef long long LL;
-LL w[105][105];
-int vis[105], m, n;
-vector<int> y[105];
-vector<pair<int, int>> C[10005];
-LL calc(int col)
+int a[100005], b[100005], vis[1000005];
+vector<int> fac[10000005];
+
+void get_fac()
 {
-    memset(vis, 0, sizeof(vis));
-    LL sum = 0;
-    for (auto now : C[col])
+    for (int i = 1; i <= 1000005; i++)
     {
-        int ni = now.first, nj = now.second;
-        for (int i = 1; i <= m; i++)
-            y[i].clear();
-        for (int i = 1; i <= m; i++)
-            if (vis[i])
-                y[vis[i]].push_back(i);
-        int yl = 1, yr = m, flag = 0;
-        for (int i = ni; i >= 1; i--)
-        {
-            vector<int>::iterator it;
-            for (it = y[i].begin(); it != y[i].end(); it++)
-            {
-                int yy = *it;
-                if (yy < nj)
-                    yl = max(yl, yy + 1);
-                else if (yy > nj)
-                    yr = min(yr, yy - 1);
-                else
-                {
-                    flag = 1;
-                    break;
-                }
-            }
-            if (flag)
-                break;
-            sum += (n - ni + 1) * (nj - yl + 1) * (yr - nj + 1);
-        }
-        vis[nj] = ni;
+        for (int j = i; j <= 1000005; j += i)
+            fac[j].push_back(i);
     }
-    return sum;
+}
+int dfs(int aa[], int n)
+{
+    if (vis[n])
+        return 0;
+    vis[n] = 1;
+    return dfs(aa, aa[n]) + 1;
 }
 int main()
 {
-    int T;
-    scanf("%d", &T);
-    while (T--)
+    int n, m, c = 1;
+    get_fac();
+    while (~scanf("%d %d", &n, &m))
     {
-        scanf("%d %d", &n, &m);
-        for (int i = 0; i <= n * m; i++)
-            C[i].clear();
-        for (int i = 1; i <= n; i++)
-            for (int j = 1; j <= m; j++)
+        memset(vis, 0, sizeof(vis));
+        for (int i = 0; i < n; i++)
+            scanf("%d", &a[i]);
+        for (int i = 0; i < m; i++)
+            scanf("%d", &b[i]);
+        vector<int> A;
+        int B[100005];
+        memset(B, 0, sizeof(B));
+        for (int i = 0; i < n; i++)
+            if (!vis[i])
+                A.push_back(dfs(a, i));
+        memset(vis, 0, sizeof(vis));
+        for (int i = 0; i < m; i++)
+            if (!vis[i])
+                B[dfs(b, i)]++;
+        int la, lb, len;
+        LL ans = 1;
+        for (int i = 0; i < A.size(); i++)
+        {
+            la = A[i];
+            LL temp = 0;
+            len = fac[la].size();
+            for (int j = 0; j < len; j++)
             {
-                scanf("%lld", &w[i][j]);
-                C[w[i][j]].push_back(make_pair(i, j));
+                lb = fac[la][j];
+                temp = (temp + lb * B[lb]) % mod;
             }
-        LL sum = 0;
-        for (int i = 0; i <= n * m; i++)
-            if (!C[i].empty())
-            {
-                sort(C[i].begin(), C[i].end());
-                sum += calc(i);
-            }
-        LL num = n * (n + 1) * m * (m + 1) / 4;
-        double ans = (sum * 1.0) / num;
-        printf("%.9lf\n", ans);
+            ans = (ans * temp) % mod;
+        }
+        printf("Case #%d: %lld\n", c++, ans);
     }
     return 0;
 }
