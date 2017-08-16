f37f0d2fe47825c13afc9a1c9c900a169ecb5505
copie <admin@copie.cn>
2017-08-16 13:31:27 +0800

    JS 提交表单出现问题

--- python/Django_Blog/myblog/codeserver/views.py
+++ python/Django_Blog/myblog/codeserver/views.py
@@ -8,4 +8,12 @@ def codeserverIndex(request,key):
     codedata = codeserver.objects.get(key=key)
     print(codedata)
     print(codedata.code)
-    return render(request,"codeserverindex.html",{'code':codedata.code,'usr':codedata.usr})
+    return render(request,"codeserverindex.html",{'code':codedata.code,'usr':codedata.usr,'date':codedata.date})
+
+def codeup(request):
+
+    return render(request,'codeupdate.html')
+
+def codeupdate(request):
+
+    return HttpResponse("hello world")
